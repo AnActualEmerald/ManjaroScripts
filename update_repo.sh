8a4268f427b6e32e02e5465bc4bf20827bd10a23
AnActualEmerald <kevinliger13@gmail.com>
2021-02-12 11:15:56 -0500

    Auto commit 02/12/21

--- update_repo.sh
+++ update_repo.sh
@@ -18,4 +18,20 @@ git commit -am "Auto commit $(date +%D)"
 git push
 
 
+#--Commit fish scripts to separate branch--#
+
+#change to the fish directory via the link in ~/bin
+cd fish_functions
+
+#add all of the .fish files (git won't add them with just `git add *`)
+git add *.fish
+
+#commit any changes
+git commit -am "Auto commit $(date +%D)"
+
+#push them to the branch of the current directory 
+#(should be on the fish branch)
+git push
+
+
 
